5a0c7931aede63a15ec268b44c05e98503e12314
leeann9867 <leeann9867@gmail.com>
2026-04-01 02:45:59 +0900

    ver 0.12c6a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
 import Main from './components/Main';
 import './index.css';
-import Footer from "./components/Footer";
 
-/**
- * 최상위 컴포넌트: 상태를 한 곳에서 관리하여 Header와 Main을 동기화함
- */
 function App() {
 
     return (
