be6cffb5b33b2d9932471afb8347d96b84635b3b
leeann9867 <leeann9867@gmail.com>
2026-05-07 13:42:45 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+// 🌟 위 주석 한 줄이 에디터의 붉은 밑줄을 싹 없애줍니다!
+
+const CACHE_NAME = 'calendar-pwa-v2';
+
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
