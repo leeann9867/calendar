9f6976c0ad3079a4a3ee2b7fbc76473c0807d161
leeann9867 <leeann9867@gmail.com>
2026-05-19 23:37:54 +0900

    ver 0.42 [minor fix]

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // server/server.js
 const app = require('./app'); // 우리가 세팅해둔 app.js를 불러옵니다!
 
-// 여기서 진짜 서버 문을 엽니다.
-app.listen(() => {
-    console.log(`🚀 서버가 실행 중입니다.`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
